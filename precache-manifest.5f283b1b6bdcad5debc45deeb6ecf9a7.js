fb4e1fabeb1fa9491b2ed04c2da93110697907ec
Ivan Krasheninnikov <exitusletalis2008@yandex.ru>
2020-07-27 12:17:53 +0300

    Updates

---
+++ precache-manifest.5f283b1b6bdcad5debc45deeb6ecf9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6de9e436827081f5ce2bc83d2cbbd77",
+    "url": "/trello-copy/index.html"
+  },
+  {
+    "revision": "39ff6f3b5165a24159eb",
+    "url": "/trello-copy/static/css/main.db81b49f.chunk.css"
+  },
+  {
+    "revision": "2eb324f45859ac2d097e",
+    "url": "/trello-copy/static/js/2.69837a5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trello-copy/static/js/2.69837a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ff6f3b5165a24159eb",
+    "url": "/trello-copy/static/js/main.4b0e075f.chunk.js"
+  },
+  {
+    "revision": "6481b1eba12ffab62fe2",
+    "url": "/trello-copy/static/js/runtime-main.33226f60.js"
+  }
+]);
